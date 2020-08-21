e99fab389dde28a87648098f9d8b21135d159f1a
joaovitorborges <55262587+joaovitorborges@users.noreply.github.com>
2020-08-20 23:19:40 -0300

    add, delete pessoas
    - add pessoa com tratamento de erros
    - delete pessoa
    ajuste de css e html

--- javascript/lista.js
+++ javascript/lista.js
@@ -1,51 +1,53 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-  $("#key").html("Key: "+localStorage.getItem("auth_key"));
-  $("#key").animate({'width':'800px'});  
+  $("#key").html(localStorage.getItem("auth_key"));   // mostra a key na tela
+  $("#key").animate({ 'width': '800px' });
 
-    fetch('http://lab.leadsoft.inf.br:5353/api/v1/People/IMC',{
-            method:'GET',
-            headers: {
-                'Accept': 'application/problem+json',                  // recebendo JSON
-                'Authorization':localStorage.getItem("auth_key")  
-              }
-        })  
-        .then(response => response.json())                 // transforma em objeto do JS
-        .then(data => {
-          if (data.HttpStatusCode == 401) {                  // erro
-           console.log("erro:Unauthorized");
-          }
-          else if(data.HttpStatusMessage){              // sucesso
-            data.Result.forEach(p => AddPessoa(p.FullName,p.Age,p.Height,p.Weigth,p.IMC),);
-          }
-        })
-        .catch(error => {                           // print erro no console
-          console.log(error);
-      });
-
-      
-  $("#BIncluir").click(function () { 
+  $("#BIncluir").click(function () {                              // botão de incluir pessoa
+    $("#header").html("Cadastrar pessoa");
+    $("#sendbtn").html("<button id = \"salvar\">Salvar</button>");
     $("#overlay").fadeIn("fast");
+
   });
 
-  $("#close").click(function () { 
+  $("#close").click(function () {                                 // botão de fechar overlay
     ClearInputs();
-    $("#warning").animate({height:'0px'});
+    $("#warning").animate({ height: '0px' });
     $("#overlay").fadeOut("fast");
+    $("#warning").removeClass('anim');
   });
-  $("#cancelar").click(function () { 
+  $("#cancelar").click(function () {
     ClearInputs();
-    $("#warning").animate({height:'0px'});
+    $("#warning").animate({ height: '0px' });
     $("#overlay").fadeOut("fast");
+    $("#warning").removeClass('anim');
   });
 
-  $("#salvar").click(function () { 
-    $("#warning").animate({height:'35px'});
-    $("#warning").addClass('anim');
-  });
-  
+  // ============================ Carregar pessoas
+  fetch('http://lab.leadsoft.inf.br:5353/api/v1/People/IMC', {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/problem+json',                  // recebendo JSON
+      'Authorization': localStorage.getItem("auth_key")
+    }
+  })
+    .then(response => response.json())                 // transforma em objeto do JS
+    .then(data => {
+      if (data.HttpStatusCode == 401) {                  // erro
+        console.log("erro:Unauthorized");
+      }
+      else if (data.HttpStatusMessage) {              // sucesso
+        data.Result.forEach(p => AddPessoa(p.Id, p.FullName, p.Age, p.Height, p.Weigth, p.IMC));
 
-  function AddPessoa(nome, idade, altura, peso, imc){
+      }
+    })
+    .catch(error => {                           // print erro no console
+      console.log(error);
+    });
+
+
+  function AddPessoa(id, nome, idade, altura, peso, imc) {  // cria linha na tabela 
+
     var tag = document.createElement("tr");
     var name = document.createElement("td");
     var age = document.createElement("td");
@@ -63,8 +65,8 @@ $(document).ready(function(){
     editBut.src = "../img/edit.png";
     deleteBut.src = "../img/delete.png";
 
-    edit.className = "edit"; 
-    del.className = "del"; 
+    edit.className = "edit";
+    del.className = "del";
 
     name.append(nome);
     age.append(idade);
@@ -84,24 +86,141 @@ $(document).ready(function(){
     tag.appendChild(IMC);
     tag.appendChild(Tedit);
     tag.appendChild(Tdel);
-    
+
+    tag.id = id;
+
     $("#Lista").append(tag);
+    $("#" + id).fadeIn("slow");
   }
 
-  function GetAge(birth){
+  function GetAge(birth) {    // transforma string de nascimento em anos
     birth = new Date(birth);
     var today = new Date();
-    var age = Math.floor((today-birth) / (365.25 * 24 * 60 * 60 * 1000));
+    var age = Math.floor((today - birth) / (365.25 * 24 * 60 * 60 * 1000));
     return age;
   }
 
-  function ClearInputs(){
+  function ClearInputs() {
     $("#nome").val("");
     $("#sobrenome").val("");
     $("#peso").val("");
     $("#altura").val("");
     $("#nascimento").val("");
+    CleanErrors();
   }
 
-  
-  });
+  function CleanErrors() {
+    $("#altura").css({ 'border': '1px solid #ccc' });
+    $("#peso").css({ 'border': '1px solid #ccc' });
+    $("#nascimento").css({ 'border': '1px solid #ccc' });
+    $("#nome").css({ 'border': '1px solid #ccc' });
+    $("#sobrenome").css({ 'border': '1px solid #ccc' });
+  }
+
+
+});
+
+$(document).on('click', '.edit', function () {
+  var ID = $(this).closest('tr').attr('id');
+  $("#header").html("Editar pessoa");
+  $("#sendbtn").html("<button id = \"atualizar\">Atualizar</button>");
+  $("#overlay").fadeIn("fast");
+});
+
+// ====================================== deletar pessoa
+$(document).on('click', '.del', function () {
+  var ID = $(this).closest('tr').attr('id');
+
+  fetch('http://lab.leadsoft.inf.br:5353/api/v1/People/' + ID, {
+    method: 'DELETE',
+    headers: {
+      'Accept': 'application/problem+json',                  // recebendo JSON
+      'Authorization': localStorage.getItem("auth_key")
+    },
+  })
+    .then(response => response.json(),
+    setTimeout(function () {
+      alert("Pessoa removida com sucesso.");
+      location.reload();
+    }, 200))                
+    .catch(error => {                           // print erro no console
+      console.log(error);
+    });
+
+
+
+});
+
+
+// ============================ ADD pessoa
+$(document).on('click', '#salvar', function () {
+  CleanErrors();
+  fetch('http://lab.leadsoft.inf.br:5353/api/v1/People', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/problem+json',                  // recebendo JSON
+      'Content-Type': 'application/json',
+      'Authorization': localStorage.getItem("auth_key")
+    },
+    body: JSON.stringify({
+      name: $("#nome").val(),  //criando JSON com usr/pswrd
+      surname: $("#sobrenome").val(),
+      dateOfBirth: $("#nascimento").val(),
+      weigth: $("#peso").val(),
+      height: $("#altura").val()
+    })
+  })
+    .then(response => response.json())                 // transforma em objeto do JS
+    .then(data => {
+      if (data.status == 400) {                  // erro
+
+        $("#warning").html("Erro nos seguintes campos.");
+        $("#warning").animate({ height: '35px' });
+        $("#warning").addClass('anim');
+        WarnErrors(data.errors);
+
+      }
+      else if (data.HttpStatusMessage) {              // sucesso
+        location.reload();
+      }
+    })
+    .catch(error => {                           // print erro no console
+      console.log(error);
+    });
+
+  function WarnErrors(errors) {
+    if (errors.height) {
+      $("#altura").css({ 'border': '2px solid red' });
+    }
+    if (errors.weigth) {
+      $("#peso").css({ 'border': '2px solid red' });
+    }
+    if (errors.dateOfBirth || errors.DateOfBirth) {
+      $("#nascimento").css({ 'border': '2px solid red' });
+    }
+    if (errors.Name || errors.name) {
+      $("#nome").css({ 'border': '2px solid red' });
+    }
+    if (errors.Surname || errors.surname) {
+      $("#sobrenome").css({ 'border': '2px solid red' });
+    }
+    if (errors.DTOPersonValidation) {
+      $("#altura").css({ 'border': '2px solid red' });
+      $("#peso").css({ 'border': '2px solid red' });
+    }
+
+    setTimeout(function () {
+      $("#warning").removeClass('anim');
+    }, 1500);
+
+  }
+
+  function CleanErrors() {
+    $("#altura").css({ 'border': '1px solid #ccc' });
+    $("#peso").css({ 'border': '1px solid #ccc' });
+    $("#nascimento").css({ 'border': '1px solid #ccc' });
+    $("#nome").css({ 'border': '1px solid #ccc' });
+    $("#sobrenome").css({ 'border': '1px solid #ccc' });
+  }
+
+});
